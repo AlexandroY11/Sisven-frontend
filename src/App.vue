71b96211e0910899d3b23b4d3dc7b3633858e001
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-05-23 19:29:12 -0500

    rutas de categorias

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/">Customers</router-link> |
-    <router-link to="/">Categories</router-link> |
+    <router-link to="/categories">Categories</router-link> |
     <router-link to="/">Pay Modes</router-link> |
     <router-link to="/">Products</router-link> |
     <router-link to="/about">About</router-link>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Categories from '@/views/Categories.vue'
+import NewCategory from '@/components/categories/NewCategory.vue'
+import EditarCategory from '@/components/categories/EditarCategory.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: Categories
+  },
+  {
+    path: '/add-category',
+    name: 'NewCategory',
+    component: NewCategory
+  },
+  {
+    path: '/editar-category/:id',
+    name: 'EditarCategory',
+    component: EditarCategory
   },
   {
     path: '/about',
