fe438f19c4f30ed68ae1d9c670301167caa9f7f9
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-05-23 19:46:52 -0500

    Lista de paymodes

--- src/views/Paymodes.vue
+++ src/views/Paymodes.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
-        <h1>Category list</h1>
-        <button @click="newComuna()" class="btn btn-outline-success mx-2">
+        <h1>Paymode list</h1>
+        <button @click="newPaymode()" class="btn btn-outline-success mx-2">
             <font-awesome-icon icon="plus" /> 
         </button>
         <table class="table">
@@ -10,21 +10,21 @@
                 <th scope="col">#</th>
                 <th scope="col">Code</th>
                 <th scope="col">Name</th>
-                <th scope="col">Municipality</th>
+                <th scope="col">Observation</th>
                 <th scope="col">Actions</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(comuna, index) in comunas" :key="index">
+                <tr v-for="(paymode, index) in paymodes" :key="index">
                     <th scope="row">{{ index+1 }}</th>
-                    <td>{{comuna.comu_codi}}</td>
-                    <td>{{comuna.comu_nomb}}</td>
-                    <td>{{comuna.muni_nomb}}</td>
+                    <td>{{paymode.id}}</td>
+                    <td>{{paymode.name}}</td>
+                    <td>{{paymode.observation}}</td>
                     <td>
-                        <button @click="deleteComuna(comuna.comu_codi)" class="btn btn-danger mx-2">
+                        <button @click="deletePaymode(paymode.id)" class="btn btn-danger mx-2">
                             <font-awesome-icon icon="trash" />
                         </button>
-                        <button @click="editComuna(comuna.comu_codi)" class="btn btn-warning mx-2">
+                        <button @click="editPaymode(paymode.id)" class="btn btn-warning mx-2">
                             <font-awesome-icon icon="pencil" /> 
                         </button>
                     </td>
@@ -40,49 +40,25 @@
     
     export default {
     
-        name: 'Comuna',
+        name: 'Paymode',
         data(){
             return{
-                comunas:[]
+                paymodes:[]
             }
         },
         methods:{
-            deleteComuna(codigo){
-                Swal.fire({
-                    title: "Are you sure you want to delete it?",
-                    text: "You won't be able to revert this!",
-                    icon: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#3085d6",
-                    cancelButtonColor: "#d33",
-                    confirmButtonText: "Yes, delete it!"
-                }).then((result) => {
-                if (result.isConfirmed) {
-                    axios.delete(`http://localhost:8000/api/comunas/${codigo}`)
-                    .then(response => {
-                        if (response.data.success){
-                            Swal.fire({
-                                title: "Deleted!",
-                                text: "Your file has been deleted.",
-                                icon: "success"
-                            });
-                            this.comunas = response.data.comunas;
-                        }
-                    });
-                }
-                });
+            
+            editPaymode(id){
+                this.$router.push({name: "EditarPaymode", params: {id: `${id}`}})
             },
-            editComuna(id){
-                this.$router.push({name: "EditarComuna", params: {id: `${id}`}})
-            },
-            newComuna(){
-                this.$router.push({name: "NewComuna"});
+            newPaymode(){
+                this.$router.push({name: "NewPaymode"});
             }
         },
         mounted(){
             axios
-                .get("http://localhost:8000/api/comunas")
-                .then(response => (this.comunas = response.data.comunas))
+                .get("http://localhost:8000/api/paymodes")
+                .then(response => (this.paymodes = response.data.paymodes))
         },
     }
     
