1f31b2bae0e278d2d5573b5d997282cf10e826a3
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-05-23 19:23:01 -0500

    Eliminar categorias

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -47,7 +47,31 @@
             }
         },
         methods:{
-            
+            deleteCategory(codigo){
+                Swal.fire({
+                    title: "Are you sure you want to delete it?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!"
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    axios.delete(`http://localhost:8000/api/categories/${codigo}`)
+                    .then(response => {
+                        if (response.data.success){
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            this.categories = response.data.categories;
+                        }
+                    });
+                }
+                });
+            },
             editCategory(id){
                 this.$router.push({name: "EditarCategory", params: {id: `${id}`}})
             },
